a454068044a34210ce07427df157d1a61139388b
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-11 15:06:50 +1200

    Define formatProperty to return specific property value

---
+++ src/formatProperty.js
@@ -0,0 +1,13 @@
+const formatProperty = (property) => {
+
+    return {
+        id: property._id,
+        askingPrice: property.askingPrice,
+        description: property.description,
+        address: property.address,
+        title: property.title,
+        img: property.img,
+    };
+};
+
+module.exports = formatProperty;
